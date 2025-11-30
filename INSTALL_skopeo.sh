d1e3e4a13f28a63ceec7acc24c7e60a68d7e5dff
cuisongliu <cuisongliu@qq.com>
2025-11-30 11:06:32 +0800

    Refactor installation script for skopeo to remove sudo and improve error handling

--- INSTALL_skopeo.sh
+++ INSTALL_skopeo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-sudo apt install -y uidmap fuse-overlayfs curl
-sudo apt purge buildah golang-github-containers-common podman skopeo -y
-sudo apt autoremove --purge  -y
-sudo sh -c "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list"
-sudo curl -fsSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/Release.key -o Release.key
-sudo apt-key add Release.key
-sudo apt-get update -y
-sudo apt-get install skopeo -y
+set -e
+apt install -y uidmap fuse-overlayfs curl
+apt purge buildah golang-github-containers-common podman skopeo -y
+apt autoremove --purge  -y
+sh -c "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list"
+curl -fsSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_22.04/Release.key -o Release.key
+apt-key add Release.key
+apt-get update -y
+apt-get install skopeo -y
 skopeo --version
 rm -f Release.key
