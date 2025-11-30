e300f0676428c1c990551bf99eb00bd3459b659b
cuisongliu <cuisongliu@qq.com>
2025-11-30 14:24:17 +0800

    Update INSTALL_skopeo.sh to suppress wget output and clean up temporary files

--- INSTALL_skopeo.sh
+++ INSTALL_skopeo.sh
@@ -2,8 +2,9 @@
 set -e
 apt update -y \
   &&  apt install -y uidmap fuse-overlayfs curl wget
-wget http://ftp.gwdg.de/pub/opensuse/repositories/home:/alvistack/xUbuntu_22.04/amd64/skopeo_1.20.0-1_amd64.deb
+wget -q http://ftp.gwdg.de/pub/opensuse/repositories/home:/alvistack/xUbuntu_22.04/amd64/skopeo_1.20.0-1_amd64.deb
 dpkg -X skopeo_1.20.0-1_amd64.deb  extract
 cp extract/usr/bin/skopeo /usr/local/bin/skopeo
 rm -rf skopeo_1.20.0-1_amd64.deb
+rm -rf extract
 skopeo --version
